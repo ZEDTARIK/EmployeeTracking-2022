6fa69fc63a768628a3c0ed3cefb45beb0adb91b6
zouhair <ettarak.zouhair@gmail.com>
2022-01-31 21:43:09 +0100

    First Step Insert Departement VALUES INTO DATABASE

---
+++ DAL/DAO/DepartmentDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+    public class DepartmentDAO: EmployeeContext
+    {
+        public static void addDepartment(DEPARTEMENT department)
+        {
+            try
+            {
+                db.DEPARTEMENTs.InsertOnSubmit(department);
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- EmployeeTracking/FrmDepartment.cs
+++ EmployeeTracking/FrmDepartment.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
+using DAL;
 
 namespace EmployeeTracking
 {
@@ -20,6 +22,23 @@ namespace EmployeeTracking
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Visible = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(txtDepartment.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Fill the field !!");
+            }
+            else
+            {
+                DEPARTEMENT department = new DEPARTEMENT();
+                department.Department = txtDepartment.Text;
+                BLL.DepartmentBLL.addDepartment(department);
+                MessageBox.Show("New Departement was Insert SuccessFully");
+            }
+               
+
         }
     }
 }
